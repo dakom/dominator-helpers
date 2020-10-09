32c9ecfcf0ecbd1a1a27b5f3f920c3e132560ef8
David Komer <david.komer@gmail.com>
2020-10-09 12:28:41 +0300

    better macros

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,3 @@ mod async_helpers;
 pub use macro_helpers::*;
 pub use async_helpers::*;
 
-
-#[cfg(test)]
-mod tests {
-    use dominator::{DomBuilder, Dom, html, events, clone, apply_methods};
-    use wasm_bindgen::prelude::*;
-    use wasm_bindgen_test::*;
-    use web_sys::HtmlImageElement;
-    use super::*; 
-
-    #[wasm_bindgen_test]
-    fn it_works() {
-        let elem = html!("<div>hello</div>", { 
-            .with_data_id!("img" => HtmlImageElement, {
-                .property("src", "foo")
-            })
-        });
-    }
-}
